69ce6fdb9a596af7a6ee906b9e6df0426146313a
Eden <edenlao@student.uts.edu.au>
2026-09-06 14:03:21 +1000

    Update StampRelay node to use Parrot1 camera topic and frame ID

--- stamp_relay.py
+++ stamp_relay.py
@@ -6,12 +6,12 @@ class StampRelay(Node):
     def __init__(self):
         super().__init__('stamp_relay')
         self.sub = self.create_subscription(
-            PointCloud2, '/husky1/camera/depth/points', self.cb, 10)
+            PointCloud2, '/parrot1/camera/depth/points', self.cb, 10)
         self.pub = self.create_publisher(
-            PointCloud2, '/husky1/camera/depth/points_stamped', 10)
+            PointCloud2, '/parrot1/camera/depth/points_stamped', 10)
 
     def cb(self, msg):
-        msg.header.frame_id = 'husky1_odom'
+        msg.header.frame_id = 'parrot1_odom'
         msg.header.stamp = self.get_clock().now().to_msg()
         self.pub.publish(msg)
 
